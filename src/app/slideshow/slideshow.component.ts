dcaa7d74f6d864812a0f12b2c3786200cddad9a4
HanamiGeto <tidu11@hotmail.de>
2023-03-08 17:00:49 +0100

    Started header menu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss']
+})
+export class SlideshowComponent implements OnInit {
+  images = ['code.jpg', 'laptop.jpg', 'javascript.jpg'];
+  headlines = [
+    'Bring engineering to the next level',
+    'with the language',
+    ''
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000)
+  }
+}
